69e53df25a68978350321b9aa646a6f61a974035
Wallflower65 <phaphamanizoneleni@gmail.com>
2026-02-25 21:08:48 +0200

    Fix syntax error in Results.js

--- my-dictionary-app/src/Results.js
+++ my-dictionary-app/src/Results.js
@@ -3,21 +3,29 @@ import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
 
 export default function Results(props) {
-if (props.results) {
-return (
-<div className="Results">
-<section>
-<h2>{props.results.word}</h2>
-{props.results.phonetics.map(function (phonetic, index) {
-return (
-<div key={index}>
-<Phonetic phonetic={phonetic} />
-</div>
-);
-})}
-</section>
+  if (props.results) {
+    return (
+      <div className="Results">
+        <section>
+          <h2>{props.results.word}</h2>
 
-} else {
-return null;
-}
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <div key={index}>
+                <Phonetic phonetic={phonetic} />
+              </div>
+            );
+          })}
+
+          {props.results.meanings.map(function (meaning, index) {
+            return (
+              <Meaning meaning={meaning} key={index} />
+            );
+          })}
+        </section>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
