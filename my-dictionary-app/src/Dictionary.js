c091018b3292795c1eba7330fc7c6d57e997595d
Wallflower65 <phaphamanizoneleni@gmail.com>
2026-02-25 21:52:17 +0200

    Updated code

--- my-dictionary-app/src/Dictionary.js
+++ my-dictionary-app/src/Dictionary.js
@@ -5,57 +5,47 @@ import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  const [keyword, setKeyword] = useState("");
-  const [results, setResults] = useState(null);
-  const [photos, setPhotos] = useState(null);
+const [keyword, setKeyword] = useState("");
+const [results, setResults] = useState(null);
+const [photos, setPhotos] = useState(null);
 
-  function handleDictionaryResponse(response) {
-    setResults(response.data[0]);
-  }
+function handleDictionaryResponse(response) {
+setResults(response.data[0]);
+}
 
-  function handlePexelsResponse(response) {
-    setPhotos(response.data.photos);
-  }
+function handleImagesResponse(response) {
+setPhotos(response.data.images);
+}
 
-  function search(event) {
-    event.preventDefault();
+function search(event) {
+event.preventDefault();
+let dictionaryApiUrl = "" + keyword;
+axios.get(dictionaryApiUrl).then(handleDictionaryResponse);
 
-    // Dictionary API
-    let dictionaryApiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(dictionaryApiUrl).then(handleDictionaryResponse);
+let imagesApiKey = "787ofe478b86fc75394a34e4469a2t40";
+let imagesApiUrl = "" + keyword + "&key=" + imagesApiKey;
+axios.get(imagesApiUrl).then(handleImagesResponse);
+}
 
-    // Pexels API
-    let pexelsApiKey = "YOUR_PEXELS_API_KEY";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
-    let headers = { Authorization: pexelsApiKey };
+function handleKeywordChange(event) {
+setKeyword(event.target.value);
+}
 
-    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
-  }
+return (
 
-  function handleKeywordChange(event) {
-    setKeyword(event.target.value);
-  }
-
-  return (
-    <div className="Dictionary">
-      <section>
-        <h1>What word do you want to look up?</h1>
-        <form onSubmit={search}>
-          <input
-            type="search"
-            onChange={handleKeywordChange}
-            placeholder="Search for a word..."
-            className="search-input"
-          />
-        </form>
-        <div className="hint">
-          i.e. coding, nursing, sunset, forest
-        </div>
-      </section>
-
-      <Results results={results} />
-      <Photos photos={photos} />
-
-    </div>
-  );
+<div className="Dictionary">
+<section>
+<h1>What word do you want to look up?</h1>
+<form onSubmit={search}>
+<input type="search" onChange={handleKeywordChange} placeholder="Search for a word..." className="search-input" />
+</form>
+<div className="hint">i.e. coding, nursing, sunset, forest</div>
+</section>
+<Results results={results} />
+<Photos photos={photos} />
+<footer className="footer">
+Coded by Your Name, open-sourced on GitHub
+</footer>
+</div>
+);
 }
